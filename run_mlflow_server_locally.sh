758d042baa9703aae2e405829ccecf3231d4a961
marsel171 <mle-user@epdssk1nj101vqqm13hv.auto.internal>
2024-10-15 10:30:58 +0000

    added files

---
+++ run_mlflow_server_locally.sh
@@ -0,0 +1,2 @@
+# mlflow server --backend-store-uri file:./mlflow_experiments_store --default-artifact-root file:./mlflow_experiments_store
+mlflow server --backend-store-uri sqlite:///mydb.sqlite --default-artifact-root file:./mlflow_experiments_store_sqlite
