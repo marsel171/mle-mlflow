023ef57958a82d2eea6870d68648665b45066c41
marsel171 <mle-user@epdssk1nj101vqqm13hv.auto.internal>
2024-10-17 11:27:52 +0000

    added new

--- run_mlflow_server_registry.sh
+++ run_mlflow_server_registry.sh
@@ -1,4 +1,5 @@
 mlflow server \
+  --registry-store-uri postgresql://$DB_DESTINATION_USER:$DB_DESTINATION_PASSWORD@$DB_DESTINATION_HOST:$DB_DESTINATION_PORT/$DB_DESTINATION_NAME\
   --backend-store-uri postgresql://$DB_DESTINATION_USER:$DB_DESTINATION_PASSWORD@$DB_DESTINATION_HOST:$DB_DESTINATION_PORT/$DB_DESTINATION_NAME\
     --default-artifact-root s3://$AWS_BUCKET_NAME \
     --no-serve-artifacts
